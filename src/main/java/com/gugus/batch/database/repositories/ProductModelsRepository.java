808432c485a37f6201266c7d7b1aac8167bf46a8
gtg <gtgyun@gmail.com>
2025-08-30 06:29:33 +0900

    fix: ProductModels 저장 변경

--- src/main/java/com/gugus/batch/database/repositories/ProductModelsRepository.java
+++ src/main/java/com/gugus/batch/database/repositories/ProductModelsRepository.java
@@ -1,16 +1,16 @@
 package com.gugus.batch.database.repositories;
 
-import com.gugus.batch.database.entities.Models;
 import com.gugus.batch.database.entities.ProductModels;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * @author : smk
- * @fileName : ModelsRepository
+ * @fileName : ProductModelsRepository
  * @date : 2025-07-22
  */
 public interface ProductModelsRepository extends JpaRepository<ProductModels, Long> {
+
+    Optional<ProductModels> findByCode(String code);
 
 }
